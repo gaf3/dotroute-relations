037458f17484fb87f4353a6cc9e4aaad38d5e2c4
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-06-01 11:07:30 -0400

    Handling Object fields

--- api/lib/dotroute_relations.py
+++ api/lib/dotroute_relations.py
@@ -5,6 +5,7 @@ Contains the Models for doTRoute example
 import time
 import relations
 
+import ipaddress
 
 class Base(relations.Model):
     """
@@ -57,3 +58,45 @@ class Thing(Base):
     items = dict
 
 relations.OneToMany(Person, Thing)
+
+
+class Meta(Base):
+    id = int
+    name = str
+    flag = bool
+    spend = float
+    stuff = list
+    things = dict
+    pull = str, {"extract": "things__for__0___1"}
+    push = str, {"inject": "stuff__-1__relations.io___1"}
+
+
+def subnet_attr(values, value):
+
+    values["address"] = str(value)
+    min_ip = value[0]
+    max_ip = value[-1]
+    values["min_address"] = str(min_ip)
+    values["min_value"] = int(min_ip)
+    values["max_address"] = str(max_ip)
+    values["max_value"] = int(max_ip)
+
+class Net(Base):
+
+    id = int
+    ip_address = str, {"extract": "ip__address"}
+    ip_value = int, {"extract": "ip__value"}
+    ip = ipaddress.IPv4Address, {
+        "attr": {"compressed": "address", "__int__": "value"},
+        "init": "address",
+        "label": "address",
+        "required": True
+    }
+    subnet = ipaddress.IPv4Network, {
+        "attr": subnet_attr,
+        "init": "address",
+        "label": "address",
+        "required": True
+    }
+
+    INDEX = "ip_value"
